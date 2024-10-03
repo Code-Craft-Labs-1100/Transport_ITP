10a2578289efd0ce126381e524140bbc2d8b8f70
AnjanaTinush <146495563+AnjanaTinush@users.noreply.github.com>
2024-10-03 18:32:37 +0530

    complete transport add

--- frontend/src/component/Transport/addtranport.js
+++ frontend/src/component/Transport/addtranport.js
@@ -6,45 +6,44 @@ import Transport from "../../images/transport.png"
 import { RiArrowGoBackLine } from "react-icons/ri";
 
 const Addmachine = () => {
-  const [name, setname] = useState("");
-  const [materialtype, setmaterialtype] = useState("");
-  const [servicedate, setservicedate] = useState("");
-  const [nextservicedate, setnextservicedate] = useState("");
-  const [description, setdescription] = useState("");
-  const [Cost, setCost] = useState("");
+  const [customername, setcustomername] = useState("");
+  const [vehicletype, setvehicletype] = useState("");
+  const [rentdate, setrentdate] = useState("");
+  const [claimdate, setclaimdate] = useState("");
+  const [rentprice, setrentprice] = useState("");
   const [error, setError] = useState("");
   const navigate = useNavigate();
 
-  const validateNextServiceDate = () => {
-    if (new Date(nextservicedate) <= new Date(servicedate)) {
-      setError("Next service date must be a future date relative to the service date.");
+  const validateclaindate = () => {
+    if (new Date(claimdate) <= new Date(rentdate)) {
+      setError("Claimdate date must be a future date relative to the Rentdate.");
       return false;
     }
     setError("");
     return true;
   };
 
-  const addmachine = async (event) => {
+  const addtransport = async (event) => {
     event.preventDefault();
 
-    if (!validateNextServiceDate()) {
+    if (!validateclaindate()) {
       return;
     }
 
-    const machine = {
-      name,
-      materialtype,
-      servicedate,
-      nextservicedate,
-      description,
-      Cost
+    const transport = {
+      customername,
+      vehicletype,
+      rentdate,
+      claimdate,
+      rentprice,
+   
     };
 
     try {
-      const result = await axios.post("http://localhost:5000/api/addmachine", machine);
+      const result = await axios.post("http://localhost:5000/api/addtransport", transport);
       console.log(result.data);
 
-      message.success('Machine added successfully!').then(() => {
+      message.success('Transport added successfully!').then(() => {
         navigate("/transportHome");
         window.location.reload()
       });
@@ -59,7 +58,7 @@ const Addmachine = () => {
         <div className="container flex justify-between items-start mr-20 mb-5">
           {/* Image container */}
           <div className="w-[600px]">
-          <Link to={"/technicalHome"}>
+          <Link to={"/transportHome"}>
           <RiArrowGoBackLine className="mt-5 ml-5 w-8 h-8" /></Link>
             <h2 className="font-bold text-dark text-3xl font-custom mt-2 mb-6 ml-8">Add new Transport</h2>
             <img src={Transport} alt="Machine" className="w-full h-auto rounded-xl mt-12 ml-44" />
@@ -67,81 +66,69 @@ const Addmachine = () => {
           </div>
           {/* Form container */}
           <div className="w-[500px] max-w-2xl bg-white shadow-xl rounded-3xl p-6 mt-20">
-            <form onSubmit={addmachine}>
+            <form onSubmit={addtransport}>
               <div className="flex flex-col mt-6">
-                <label htmlFor="machineName" className="text-gray-700 font-bold mb-2">Machine name</label>
+                <label htmlFor="machineName" className="text-gray-700 font-bold mb-2">Customer name</label>
                 <input
                   type="text"
-                  value={name}
-                  onChange={(e) => setname(e.target.value)}
+                  value={customername}
+                  onChange={(e) => setcustomername(e.target.value)}
                   placeholder="Machine name"
                   required
                   className="p-2 block w-full rounded-xl bg-gray-100 text-black border-none placeholder-gray-400 placeholder-opacity-50 font-custom text-md"
                 />
               </div>
               <div className="flex flex-col mt-6">
-                <label htmlFor="machineName" className="text-gray-700 font-bold mb-2">Material type</label>
+                <label htmlFor="machineName" className="text-gray-700 font-bold mb-2">Vehicle type</label>
                 <select
-              value={materialtype}
-              onChange={(e) => setmaterialtype(e.target.value)}
+              value={vehicletype}
+              onChange={(e) => setvehicletype(e.target.value)}
               required
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-xl block w-full p-2.5"
             >
               <option  value="">Select here</option>
-              <option value="type_A">HDPE</option>
-              <option value="type_B">PVC</option>
-              <option value="type_C">Polypropylene</option>
-              <option value="type_D">Polystyrene</option>
-              <option value="type_E">Acrylic</option>
+              <option value="car">Car</option>
+              <option value="van">Van</option>
+              <option value="motobick">Motobick</option>
             </select>
               </div>
               <div className="flex flex-col mt-4">
-                <label htmlFor="serviceDate" className="text-gray-700 font-bold mb-2">Service date</label>
+                <label htmlFor="serviceDate" className="text-gray-700 font-bold mb-2">Rent date</label>
                 <input
                   type="date"
-                  value={servicedate}
-                  onChange={(e) => setservicedate(e.target.value)}
+                  value={rentdate}
+                  onChange={(e) => setrentdate(e.target.value)}
                   required
                   className="p-2 block w-full rounded-xl bg-gray-100 border-none focus:outline-none placeholder-gray-500 placeholder-opacity-50 font-custom text-md"
                 />
               </div>
               <div className="flex flex-col mt-4">
-                <label htmlFor="nextServiceDate" className="text-gray-700 font-bold mb-2">Next service date</label>
+                <label htmlFor="nextServiceDate" className="text-gray-700 font-bold mb-2">Claim date</label>
                 <input
                   type="date"
-                  value={nextservicedate}
-                  onChange={(e) => setnextservicedate(e.target.value)}
+                  value={claimdate}
+                  onChange={(e) => setclaimdate(e.target.value)}
                   required
                   className="p-2 block w-full rounded-xl bg-gray-100 border-none focus:outline-none placeholder-gray-500 placeholder-opacity-50 font-custom text-md"
                 />
                 {error && <p className="text-red-500 mt-2">{error}</p>}
               </div>
               <div className="flex flex-col mt-4">
-                <label htmlFor="cost" className="text-gray-700 font-bold mb-2">Cost</label>
+                <label htmlFor="cost" className="text-gray-700 font-bold mb-2">Rent price</label>
                 <input
                   type="number"
-                  value={Cost}
-                  onChange={(e) => setCost(e.target.value)}
+                  value={rentprice}
+                  onChange={(e) => setrentprice(e.target.value)}
                   required
                   placeholder='Cost'
                   className="p-2 block w-full rounded-xl bg-gray-100 border-none focus:outline-none placeholder-gray-500 placeholder-opacity-50 font-custom text-md"
                 />
               </div>
-              <div className="flex flex-col mt-6">
-                <label htmlFor="description" className="text-gray-700 font-bold mb-2">Description</label>
-                <textarea
-                  value={description}
-                  onChange={(e) => setdescription(e.target.value)}
-                  required
-                  rows="4"
-                  className="p-2.5 block w-full text-sm text-gray-500 rounded-xl bg-gray-100 dark:border-gray-100 placeholder-gray-500 placeholder-opacity-50"
-                  placeholder="Summary of the service"
-                />
-              </div>
+              
               <div className="mt-8 mb-2">
                 <button
                   type="submit"
-                  className="text-white bg-green-500 block w-full font-semibold rounded-md text-md px-5 py-2.5 text-center transition-transform duration-300 ease-in-out transform hover:scale-105"
+                  className="text-white bg-blue-500 block w-full font-semibold rounded-md text-md px-5 py-2.5 text-center transition-transform duration-300 ease-in-out transform hover:scale-105"
                 >
                   Confirm
                 </button>
